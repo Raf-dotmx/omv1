607e0fec93fd18dcecfd2eb1756515b7c77a971a
Ra <oso_imartinez@hotmail.com>
2026-04-23 18:39:25 -0600

    Add favicon images for improved branding and user experience
    - Added favicon-32.png for high-resolution display support.
    - Added favicon.ico for compatibility with various browsers and platforms.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '../assets/Logo.svg';
+import logo from '../assets/logo.svg';
 
 //PascalCasing
 function Header() {
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
 function Hero() {
     return (
-        <div className="hero">
-            <h1>Osvaldo Martinez</h1>
-            <h3>Software Engineer</h3>
-            <p><span className="highlight" >Fullstack Developer</span > con experiencia en Angular, React, Node.js y bases de datos SQL. Me enfoco en construir aplicaciones web eficientes, escalables y fáciles de mantener.</p>
+        <div className="hero-bg">
+            <div className="hero" data-aos="zoom-in-up" data-aos-duration="1000" data-anchor-placement="center-bottom" >
+                <h1>Osvaldo Martinez</h1>
+                <h3>Software Engineer</h3>
+                <p><span className="highlight" >Fullstack Developer</span > con experiencia en Angular, React, Node.js y bases de datos SQL. Me enfoco en construir aplicaciones web eficientes, escalables y fáciles de mantener.</p>
+            </div>
         </div>
     );
 }

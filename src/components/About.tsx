1fa527cf841a419c251498a760cab80e864a99cd
Ra <oso_imartinez@hotmail.com>
2026-04-23 20:15:23 -0600

    Improved CSS styles for About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,12 @@
 
 function About() {
   return (
-    <section id="about" className="container about" data-aos="fade-up" data-aos-delay="300">
+    <section id="about" className="container about flex" data-aos="fade-up" data-aos-delay="300">
       <h2>Sobre mí</h2>
       <p>
         Soy ingeniero de software enfocado en el desarrollo fullstack, con experiencia en la creación de aplicaciones web utilizando tecnologías como Angular, React y Node.js. <br />
         También he trabajado con bases de datos SQL y desarrollo de APIs, buscando siempre construir soluciones eficientes y bien estructuradas. <br /><br />
-        De igual forma, he trabajado con tecnologías como Python, C#, Tableau, Unity y Blender, lo que me permite tener una visión más amplia en el desarrollo de soluciones. <br />
+        De igual forma, he trabajado con tecnologías como Python, C#, Tableau, Unity y Blender, tengo bastante conocimiento en otros ambitos de software. <br />
         Actualmente sigo fortaleciendo mis habilidades en React y arquitecturas modernas.
       </p>
     </section>
